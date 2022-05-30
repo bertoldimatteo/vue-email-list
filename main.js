2e6fb46890ab9343aa38014f67ffdc47c9849047
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-30 14:32:10 +0200

    aggiunta funzione + check stampa mail su html

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let app = new Vue({
     el: '#root',
     data: {
+        mail: '',
+    },
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((obj) => {
 
+        this.mail = obj.data.response;
+        });
     }
 })
